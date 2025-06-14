a8537e57b6a2a11fd8d4bb638521ed39ba1fd197
rzqmhb <roziqmahbubi22@gmail.com>
2025-06-14 12:30:09 +0700

    init

---
+++ dcgan.py
@@ -0,0 +1,226 @@
+import argparse
+import os
+import numpy as np
+import math
+
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+from fid_evaluation import fid_score
+
+os.makedirs("images", exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--evaluate_interval", type=int, default=20, help="interval between generator evaluation using FID")
+parser.add_argument("--outdir", type=str, help="Training output directory")
+parser.add_argument("--dataset_dir", type=str, help="Image dataset directory")
+opt = parser.parse_args()
+print(opt)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(opt.latent_dim, 1024, 4, 1, 0),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(64, opt.channels, 4, 2, 1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        z = z.view(z.size(0), opt.latent_dim, 1, 1)
+        return self.model(z)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(opt.channels, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(512, 1, 4, 1, 0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        out = self.model(img)
+        return out.view(-1, 1)
+
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+
+# Load image dataset
+transform = transforms.Compose([
+    transforms.Resize(opt.img_size),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5]*3, [0.5]*3),
+])
+
+# Configure image data loader
+imagenet_dataset = datasets.ImageFolder(opt.dataset_dir, transform=transform)
+dataloader = DataLoader(
+    imagenet_dataset,
+    batch_size=opt.batch_size,
+    shuffle=True,
+    num_workers=2,
+    pin_memory=True,
+    persistent_workers=True
+)
+
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+for epoch in range(1, opt.n_epochs):
+    for i, (imgs, _) in enumerate(dataloader, 1):
+
+        # Adversarial ground truths
+        valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        if i % 4000 == 0:
+            kimg = ((i + ((epoch - 1) * len(dataloader)) ) // 1000)
+
+            print(
+                "[Epoch %d/%d] [kimg %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, kimg, ((len(dataloader) * opt.n_epochs) // 1000), d_loss.item(), g_loss.item())
+            )
+
+            if (i // 1000) % opt.evaluate_interval == 0:
+                # FID evaluation
+                n_sample = 50_000 if len(dataloader) >= 50_000 else len(dataloader)
+                fid_score(
+                    kimg=kimg,
+                    dir=opt.outdir,
+                    generator=generator,
+                    latent_dim=opt.latent_dim,
+                    real_dir=opt.dataset_dir,
+                    device=device,
+                    n_samples=n_sample,
+                    batch_size=opt.batch_size
+                )
+
+                # Saving Generator
+                out = os.path.join(opt.outdir, f"generator_{kimg}.pt")
+
+                device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                z = torch.randn(1, opt.latent_dim, 1, 1).to(device)
+                traced_gen = torch.jit.trace(generator.to(device), z)
+                traced_gen.save(out)
---
+++ fid_evaluation.py
@@ -0,0 +1,55 @@
+import torch
+import os
+import torchvision.utils as vutils
+from torchvision import transforms
+from tqdm import tqdm
+import json
+from pytorch_fid import fid_score as fid_score_func 
+
+def generate_fake_images(generator, latent_dim, save_dir, n_samples=50000, batch_size=100, device='cuda'):
+    os.makedirs(save_dir, exist_ok=True)
+    generator.eval().to(device)
+    
+    n_batches = n_samples // batch_size
+    img_id = 0
+
+    for _ in tqdm(range(n_batches)):
+        z = torch.randn(batch_size, latent_dim, 1, 1).to(device)
+        with torch.no_grad():
+            fakes = generator(z).detach().cpu()  
+            fakes = fakes.add(1).div(2).clamp(0, 1)  
+        
+        for img in fakes:
+            vutils.save_image(img, os.path.join(save_dir, f"{img_id:05}.png"))
+            img_id += 1
+
+def dump_json_data(json_dir, kimg, fid):
+    if not os.path.exists(json_dir):
+        with open(json_dir, 'w') as f:
+            json.dump({"fid_evaluation": []}, f, indent=4)
+
+    with open(json_dir, 'r') as f:
+        data = json.load(f)
+    
+    new_entry = {"kimg": kimg, "fid": fid}
+    data["fid_evaluation"].append(new_entry)
+
+    with open(json_dir, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def fid_score(kimg, dir, generator, latent_dim, real_dir, device, n_samples=50000, batch_size=100):
+    from tempfile import TemporaryDirectory
+
+    with TemporaryDirectory() as tmp_fake_dir:
+        generate_fake_images(generator, latent_dim, tmp_fake_dir, n_samples, batch_size, device)
+        
+        score = fid_score_func.calculate_fid_given_paths(
+            [real_dir, tmp_fake_dir],
+            batch_size=batch_size,
+            device=device,
+            dims=2048,
+        )
+
+        out = os.path.join(dir, 'FID_Evaluation.json')
+        
+        dump_json_data(out, kimg, score)
